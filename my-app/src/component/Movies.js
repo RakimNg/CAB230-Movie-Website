6eb421e2dfd77f6dbc25c47dbc5ca47210ed7f3b
RakimNg <105621141+RakimNg@users.noreply.github.com>
2023-05-09 17:58:45 +1000

    Movie Library without filtering function

--- my-app/src/component/Movies.js
+++ my-app/src/component/Movies.js
@@ -1,13 +1,107 @@
+
 import React, { useEffect, useState } from 'react';
+import { Nav, NavItem, NavLink, Container, Row, Col, Card, CardGroup, CardBody, CardTitle, CardText, CardSubtitle, Button, CardImg, UncontrolledCarousel, Label, Input } from 'reactstrap';
 import { Navigation } from './base';
-
-export function MoviesLib() {
+function Headline(props) {
     return (
+        <div className='p-5 bg-light'>
+            <div className='bg-white shadow border'>
 
-        < div >
-            <Navigation />
-            <p>Hello World</p>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Year</th>
+                            <th>imdbRating</th>
+                            <th>Classification</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+                        {props.data.map((prop) => (
+
+                            <tr key={prop.imdbID} >
+
+                                <td>{prop.title}</td>
+                                <td>{prop.year}</td>
+                                <td>{prop.imdbRating}</td>
+                                <td>{prop.classification}</td>
+                                <td>Inactive link</td>
+                            </tr>
+
+                        ))}
+                    </tbody>
+                </table >
+            </div >
+
+
 
         </div >
+
+
     )
+}
+const useData = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [headlines, setHeadlines] = useState([]);
+
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await fetch('http://sefdb02.qut.edu.au:3000/movies/search?title=Star%20War');
+                const data = await res.json();
+                const dataArray = data;
+
+                setHeadlines(dataArray);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        getData();
+    }, []);
+
+    return { loading, error, headlines };
+};
+
+
+
+export function MoviesLib() {
+
+
+    const { loading, error, headlines } = useData();
+    if (loading) {
+        return <p>loading...</p>;
+    }
+
+    if (error) {
+        return <p>Something went wrong: {error.message}</p>;
+    }
+
+    return (<div>
+
+        <Navigation />
+        <header>Welcome to movies library</header>
+
+        <Headline{...headlines} />
+        {/* {headlines.map((headline) => (
+            <Headline
+                key={headline.imdbID}
+                title={headline.title}
+                year={headline.year}
+                imdbRating={headline.imdbRating}
+                classification={headline.classification}
+            />
+        ))} */}
+        {/* <p>{title}</p> */}
+
+    </div>)
+
 }
