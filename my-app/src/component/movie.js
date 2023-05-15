1b43749ff9340ff4c81c73ae31ff7a21fdd011df
RakimNg <105621141+RakimNg@users.noreply.github.com>
2023-05-15 22:35:25 +1000

    view button
    after user click view button, it will navigate to the specification page

--- my-app/src/component/movie.js
+++ my-app/src/component/movie.js
@@ -3,29 +3,32 @@ import { Navigation } from './base';
 import { Button } from 'reactstrap';
 import React, { useEffect, useState } from 'react';
 export const MoviePage = () => {
-    const [records, setRecords] = useState([])
+    // const [records, setRecords] = useState([])
     const [data, setData] = useState([])
     const [headlines, setHeadlines] = useState([])
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState()
-    const handleInputChange = (event) => {
-        setRecords(event.target.value);
-        console.log(typeof event.target.value)
-        console.log(records)
-    }
-    const handleSubmit = (event) => {
-        // setLoading(true)
-        event.preventDefault();
-        setData(records)
-        // fetchData(data)
+    const ID = localStorage.getItem("imdbID")
+    // setData(ID)
+    // const handleInputChange = (event) => {
+    //     setRecords(event.target.value);
+    //     console.log(typeof event.target.value)
+    //     console.log(records)
+    // }
+    // const handleSubmit = (event) => {
+    //     // setLoading(true)
+    //     event.preventDefault();
+    //     setData(records)
+    //     // fetchData(data)
 
 
-    }
+    // }
     useEffect(
         () => {
+            console.log("hello")
             const fetchData = async () => {
                 try {
-                    const response = await fetch(`http://sefdb02.qut.edu.au:3000/movies/data/${encodeURIComponent(data)}`)
+                    const response = await fetch(`http://sefdb02.qut.edu.au:3000/movies/data/${encodeURIComponent(ID)}`)
                     const data1 = await response.json();
                     // const country = await data1.country;
 
@@ -43,7 +46,7 @@ export const MoviePage = () => {
 
             }
             fetchData();
-        }, [data]
+        }, []
 
     )
     const join = (array) => {
@@ -56,7 +59,6 @@ export const MoviePage = () => {
         const principals_arr = headlines?.principals
         const ratings_arr = headlines?.ratings
         const genres_arr = headlines?.genres
-        console.log(principals_arr)
         if (!headlines || !principals_arr || !ratings_arr || !genres_arr) {
             return null;
         }
@@ -116,41 +118,33 @@ export const MoviePage = () => {
     if (error) {
         return <p>Something went wrong: {error.message}</p>;
     }
-    const ID = localStorage.getItem("imdbID")
-    // if (!ID) {
+
+
     return (
         <div>
             <Navigation />
 
 
-            <div className='p-5 bg-light'>
+            {/* <div className='p-5 bg-light'>
 
-                <form onSubmit={handleSubmit}>
-                    <div style={{ display: 'flex' }}>
-                        <input type="text" className='form-control' value={records} onChange={handleInputChange} placeholder='Search movie imdbID' />
-                        <button type="submit">Search</button>
-                    </div>
-                </form>
+                    <form onSubmit={handleSubmit}>
+                        <div style={{ display: 'flex' }}>
+                            <input type="text" className='form-control' value={records} onChange={handleInputChange} placeholder='Search movie imdbID' />
+                            <button type="submit">Search</button>
+                        </div>
+                    </form> */}
 
-                <div>
-                    {mapData()}
+            <div>
+                {mapData()}
+            </div>
+            {/* {} */}
+            {/* {headlines && <p>{headlines.title}</p> && <p>{headlines.year}</p> && <p>{headlines.country}</p> && mapPrincipals()} */}
 
-                </div>
-                {/* {} */}
-                {/* {headlines && <p>{headlines.title}</p> && <p>{headlines.year}</p> && <p>{headlines.country}</p> && mapPrincipals()} */}
-
-            </div >
-        </div>
+        </div >
+        // </div> */}
 
     )
-    // }
-    // else if (ID) {
-    //     setData(ID)
-    //     mapData()
-    //     localStorage.setItem(null, "imdbID");
 
-
-    // }
 
 }
 
